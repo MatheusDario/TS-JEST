696a1af8b441a8dc3c58d133bbe45b26abfd8a5f
Matheus Dario <dario.matheus97@gmail.com>
2023-11-11 17:39:25 -0300

    about spyOn

--- src/services/persistency.spec.ts
+++ src/services/persistency.spec.ts
@@ -1,6 +1,25 @@
-describe('first test', () => {
-  it('shuld return a number one', () => {
-    const numberOne = 1;
-    expect(numberOne).not.toBe(String);
+import { Persistency } from './persistency';
+
+describe('Persistency', () => {
+  afterEach(() => jest.clearAllMocks());
+
+  it('Persistency shout be return undefined', () => {
+    const sut = new Persistency();
+
+    expect(sut.saveOrder()).toBeUndefined();
+  });
+
+  it('Shuld be called console.log once', () => {
+    const sut = new Persistency();
+    const consoleSpy = jest.spyOn(console, 'log');
+    sut.saveOrder();
+    expect(consoleSpy).toHaveBeenCalledTimes(1);
+  });
+
+  it('Shuld call console.log with "Seu pedido foi salvo"', () => {
+    const sut = new Persistency();
+    const consoleSpy = jest.spyOn(console, 'log');
+    sut.saveOrder();
+    expect(consoleSpy).toHaveBeenCalledWith('Seu pedido foi salvo');
   });
 });
